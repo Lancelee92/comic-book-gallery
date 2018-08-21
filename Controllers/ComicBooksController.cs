4962cbee38d80b250e10393920412caccb22214e
yc <lancelee92@gmail.com>
2018-08-21 17:28:12 +0800

    Added ActionResult

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -8,6 +8,15 @@ namespace comic_book_gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday){
+                return new RedirectResult("/");
+            }
+            return new ContentResult(){
+                Content = "Hello from the comic book controller"
+            };
+            //return "Hello from the comic book controller";
+        }
     }
 }

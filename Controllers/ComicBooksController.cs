7aedba523fa037e0998dbbbc2f64482d01686620
yc <lancelee92@gmail.com>
2018-08-21 21:37:48 +0800

    Added new comic book detail view

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -10,12 +10,13 @@ namespace comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday){
+            return View();
+            /*if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday){
                 return new RedirectResult("/");
             }
             return new ContentResult(){
                 Content = "Hello from the comic book controller"
-            };
+            };*/
             //return "Hello from the comic book controller";
         }
     }
